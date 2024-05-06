0160805932207272f0233937765c76ab11e21d82
MrCake4 <winstonjreichelt@gmail.com>
2024-05-06 20:01:21 +0200

    Task 03

--- src/Histogram.java
+++ src/Histogram.java
@@ -11,6 +11,8 @@ import java.util.logging.Logger;
 
 public class Histogram {
     private static int[] frequencies;
+
+    // TASK 01
     private static final Logger logger = Logger.getLogger(Histogram.class.getName());
 
     public static Character getNextCharacter(String filePath) {
@@ -32,6 +34,7 @@ public class Histogram {
         return null;
     }
 
+    // TASK 02
     public void writeStringToFile(String filePath, String content) {
         try (FileWriter writer = new FileWriter(filePath)) {
             writer.write(content);
@@ -42,4 +45,79 @@ public class Histogram {
         }
     }
 
+    public void writeIntegerToFile(String filePath, Integer number) {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            writer.write(number.toString());
+            System.out.println("'" + number + "'"+ " successfully written to the file: " + filePath);
+
+        } catch (IOException e) {
+            logger.severe("An error occurred:");
+            logger.severe(e.toString());
+        }
+    }
+
+    public void createFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+
+}}
+    // TASK 03
+    private static boolean isASCIILetters(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+
+    private static int getNormalizedIndex(char c) {
+        if (c >= 'a' && c <= 'z') {
+            return c - 'a' + 26;
+        } else if (c >= 'A' && c <= 'Z'){
+            return c - 'A';
+        } else {
+            return -1;
+        }
+    }
+
+    public static void countCharacterFrequencies(String inputFilePath, String outputFilePath) {
+        int[] frequencies = new int[52];
+
+        buffer(inputFilePath, frequencies);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+            for (char c = 'A'; c <= 'Z'; c++) {
+                writer.write(c + ": " + frequencies[c - 'A'] + "\n");
+            }
+            for (char c = 'a'; c <= 'z'; c++) {
+                writer.write(c + ": " + frequencies[c - 'a' + 26]  + "\n");
+            }
+        } catch (IOException e) {
+            logger.severe("An error occurred:");
+            logger.severe(e.toString());
+        }
+
+        System.out.println("Character frequencies written to: " + outputFilePath);
+    }
+
+    private static void buffer(String inputFilePath, int[] frequencies) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
+            int nextCharInt;
+            while ((nextCharInt = reader.read()) != -1) {
+                char nextChar = (char) nextCharInt;
+
+                if (isASCIILetters(nextChar)) {
+                    int index = getNormalizedIndex(nextChar);
+                    frequencies[index]++;
+                }
+            }
+        } catch (IOException e) {
+            logger.severe("An error occurred:");
+            logger.severe(e.toString());
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,11 @@ public class Main {
         }
 
         // TASK 02
-        histogram.writeStringToFile("files/string.txt", "TESTING STRING");
+        histogram.writeStringToFile("files/file.txt", "TESTING STRING");
+        histogram.writeIntegerToFile("files/file.txt", 123);
+        histogram.createFile("files/testFile.txt");
+
+        // TASK 03
+        histogram.countCharacterFrequencies("files/script.txt", "files/ASCIILetters.txt");
     }
 }
